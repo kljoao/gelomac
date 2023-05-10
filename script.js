810f2d9e257f83bd8c7a640b1b25c9156f8d2c68
João Luis Bernardo Ramos <joao.ramos@sicoobcecremef.local>
2023-05-10 15:00:41 -0300

    menu-mobile

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+// SCRIPT DROPMENU
+
+const btnMenu = document.getElementById('menu-drop');
+const menuNav = document.getElementById('nav');
+
+function toggleMenu(){
+    menuNav.classList.toggle('ativo')
+}
+
+
+btnMenu.addEventListener('click', toggleMenu);
+
+// SCRIPT DROPMENU
+
+
+
+
+
+// SCRIPT MÁQUINA DE ESCREVER
+
 const titulo = document.getElementById('titulo')
 
-function typeWriter(elemento){
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ''
+function typeWriter(e){
+    const textoArray = e.innerHTML.split('');
+    e.innerHTML = ''
     textoArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 65 * i)
+        setTimeout(() => e.innerHTML += letra, 65 * i)
     })
 }
 
 typeWriter(titulo);
+
+// SCRIPT MÁQUINA DE ESCREVER
